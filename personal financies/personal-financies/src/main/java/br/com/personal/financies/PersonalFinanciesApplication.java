fed74c51a83d7c7e60c30d0550cd42ca9dd233e9
Luis Felipe <llipeandrade34@gmail.com>
2022-02-02 20:01:16 -0300

    Spring Initializr

---
+++ personal financies/personal-financies/src/main/java/br/com/personal/financies/PersonalFinanciesApplication.java
@@ -0,0 +1,13 @@
+package br.com.personal.financies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanciesApplication.class, args);
+	}
+
+}
